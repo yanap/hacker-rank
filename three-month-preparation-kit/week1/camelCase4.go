5bd4a9347d93bb9d1a97a420885a6d4f6dccee03
yanap <yanap1214@gmail.com>
2022-08-12 21:48:31 +0900

    812

--- three-month-preparation-kit/week1/camelCase4.go
+++ three-month-preparation-kit/week1/camelCase4.go
@@ -1,28 +1,29 @@
 package main
+
 import (
-    "fmt"
-    "bufio"
-    "strings"
-    "os"
+	"bufio"
+	"os"
+	"strings"
+	"unicode"
 )
 
-
 func main() {
- //Enter your code here. Read input from STDIN. Print output to STDOUT
-    sc := bufio.NewScanner(os.Stdin)
-    for sc.Scan() {
-        t:= sc.Text()
-        a := strings.Split(t, ";")
-        if a[0] == "S" {
-            if a[1] == "M" {
-                m := strings.Trim(a[2], "()")
-                r := strings.SplitAfter(m, "")
-                for _, v := range r {
-                    
-                }
-            }
-        } else if a[0] == "C" {
-            
-        }
-    }
+	//Enter your code here. Read input from STDIN. Print output to STDOUT
+	sc := bufio.NewScanner(os.Stdin)
+	for sc.Scan() {
+		t := sc.Text()
+		a := strings.Split(t, ";")
+		if a[0] == "S" {
+			if a[1] == "M" {
+				m := strings.Trim(a[2], "()")
+				for _, v := range m {
+					if unicode.IsUpper(v) {
+					} else {
+					}
+				}
+			}
+		} else if a[0] == "C" {
+
+		}
+	}
 }
